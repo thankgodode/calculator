f7ae190474ff8df847cc41cf41c4ce61a403f501
Dtech101 <117050835+thankgodode@users.noreply.github.com>
2023-03-16 01:04:06 +0100

    Added files

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const btns = document.querySelectorAll(".num");
+const label = document.querySelector("p");
+const equate = document.querySelector(".equate");
+const del = document.querySelector(".delete");
+const clear = document.querySelector(".clear");
+
+btns.forEach((btnEl) => {
+  btnEl.addEventListener("click", function () {
+    if (label.innerHTML == 0) {
+      label.innerHTML = "";
+    }
+    label.innerHTML += btnEl.innerHTML;
+  });
+});
+
+equate.addEventListener("click", function () {
+  try {
+    let equ = eval(label.innerHTML);
+    label.innerHTML = equ;
+  } catch (error) {
+    label.innerHTML = "Syntax error";
+  }
+});
+
+let x = 0;
+
+del.addEventListener("click", function () {
+  x = 1;
+  let del = label.innerHTML;
+  let sliceDel = del.slice(0, del.length - x);
+  label.innerHTML = sliceDel;
+  console.log(sliceDel);
+  if (label.innerHTML == "") {
+    label.innerHTML = "0";
+  }
+});
+
+clear.addEventListener("click", function () {
+  label.innerHTML = "0";
+});
