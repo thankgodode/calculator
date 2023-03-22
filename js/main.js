414d6bdb46b6383fcdd1dbca746594237ca52bb4
Thankgod Ode <117050835+thankgodode@users.noreply.github.com>
2023-03-22 04:50:37 +0100

    sdsd

--- js/main.js
+++ js/main.js
@@ -3,11 +3,16 @@ const label = document.querySelector("p");
 const equate = document.querySelector(".equate");
 const del = document.querySelector(".delete");
 const clear = document.querySelector(".clear");
+const errorM = document.querySelector(".error-message");
+
 
 btns.forEach((btnEl) => {
   btnEl.addEventListener("click", function () {
-if(label.innerHTML == 0) label.innerHTML=""
+    if (label.innerHTML == "0") label.innerHTML = "";
+    if (btnEl.innerHTML == "." && label.innerHTML == 0) label.innerHTML = 0;
+    if (label.innerHTML == "span") label.innerHTML = btnEl.innerHTML;
     label.innerHTML += btnEl.innerHTML;
+    console.log(label.innerHTML);
   });
 });
 
@@ -16,7 +21,7 @@ equate.addEventListener("click", function () {
     let equ = eval(label.innerHTML);
     label.innerHTML = equ;
   } catch (error) {
-    label.innerHTML = "Syntax error";
+    label.innerHTML = `<span class="error-message" style="color: red; font-family: Segoe UI ">Syntax error</span>`;
   }
 });
 
@@ -27,12 +32,15 @@ del.addEventListener("click", function () {
   let del = label.innerHTML;
   let sliceDel = del.slice(0, del.length - x);
   label.innerHTML = sliceDel;
-  console.log(sliceDel);
   if (label.innerHTML == "") {
-    label.innerHTML = 0
+    label.innerHTML = "0";
+  }
+  if (errorM) {
+    del.style.color = "#ccc";
+    del.ariaDisabled;
   }
 });
 
 clear.addEventListener("click", function () {
-  label.innerHTML = 0
-}); 
+  label.innerHTML = "0";
+});
